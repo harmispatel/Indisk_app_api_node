727157b3374498a53c9571b524717c913004d05f
vijay2528 <jadavvijayr@gmail.com>
2025-04-04 11:08:23 +0530

    make some changes

--- routes/Routes.js
+++ routes/Routes.js
@@ -8,8 +8,9 @@ const {
   forgotPassword,
   resetPassword,
 } = require("../controllers/AuthLoginController");
+const {authenticateToken} = require("../middlewares/auth")
 
-router.get("/auth-user-list", getAuthUsers);
+router.get("/auth-user-list", authenticateToken, getAuthUsers);
 router.post("/login", loginUser);
 router.post("/signup", registerUser);
 router.post("/forgot-password", forgotPassword);
